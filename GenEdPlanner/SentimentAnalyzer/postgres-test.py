97e0f59c3b40beb8098ff44836c929ac09ea9f57
Sam <kulkarni.samarth@yahoo.com>
2023-04-02 03:18:10 -0400

    Updating CSV and Syntax to allow for postgres

--- GenEdPlanner/SentimentAnalyzer/postgres-test.py
+++ GenEdPlanner/SentimentAnalyzer/postgres-test.py
@@ -1,48 +1,52 @@
 from pathlib import Path
 
-import pymysql
 import pandas as pd
 import psycopg2 as psycopg2
 from dotenv import load_dotenv
 import os
-'''
+import numpy
+from psycopg2._psycopg import AsIs
+
 dotenv_path = Path('Environment Variables/.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-mydb = psycopg2.connect(
+conn = psycopg2.connect(
     host=os.getenv('host'),
     user=os.getenv('user'),
     password=os.getenv('password'),
     database=os.getenv('database'),
     port=os.getenv('port')
 )
-'''
-conn=pymysql.connect(host='localhost',user='root',password='Mayank.123')
-cur=conn.cursor()
 
+cur = conn.cursor()
 
-
-
-cur.execute("create database if not exists hackathonPSU;")
-cur.execute("use hackathonPSU;")
+# cur.execute("create database if not exists hackathonPSU;")
+# cur.execute("use hackathonPSU;")
 cur.execute("drop table subjects;")
-cur.execute("CREATE TABLE IF NOT EXISTS subjects(Course VARCHAR(255), Code VARCHAR(10), Credits INT, GA INT, GHW INT, GH INT, GN INT, GS INT, Rating VARCHAR(15), ABSRating INT);")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS subjects(Course VARCHAR(255), Code VARCHAR(10), Credits INT, GA INT, GHW INT, GH INT, GN INT, GS INT, Rating VARCHAR(15), ABSRating INT);")
 
 
-def insert(filename,types):
-    df_arts=pd.read_csv(filename)
+def insert(filename, types, lst1):
+    df_arts = pd.read_csv(filename)
     print(df_arts)
     for i in range(len(df_arts)):
         print(df_arts.loc[i].at["Code"])
-        cur.execute('select * from subjects where code="'+df_arts.loc[i].at["Code"]+'";')
-        r=cur.rowcount
-        if r==0:
-            cur.execute("insert into subjects values('"+df_arts.loc[i].at['Course']+"','"+df_arts.loc[i].at['Code']+"','"+str(df_arts.loc[i].at['Credits'])+"',1,0,0,0,0,NULL,NULL);")
+        cur.execute("select * from subjects where code='" + df_arts.loc[i].at["Code"] + "';")
+        r = cur.rowcount
+        if r == 0:
+            cur.execute(
+                "insert into subjects (Course, Code, Credits, GA, GHW, GH, GN, GS, Rating, ABSRating) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, NULL, NULL);",
+                (df_arts.loc[i].at['Course'], df_arts.loc[i].at['Code'], AsIs(df_arts.loc[i].at['Credits']), lst1[0], lst1[1],
+                 lst1[2], lst1[3], lst1[4]))
             conn.commit()
         else:
-            cur.execute("update subjects set "+types+"=1 where Code='"+df_arts.loc[i].at["Code"]+"';")  
+            cur.execute("update subjects set " + types + "=1 where Code='" + df_arts.loc[i].at["Code"] + "';")
             conn.commit()
 
-lst=[["arts_gened.csv","GA"],["humanities.csv","GH"],["health_wellness.csv","GHW"],["nat_sci.csv","GN"],["social_behavior.csv","GS"]]
+
+lst = [["arts_gened.csv", "GA", [1, 0, 0, 0, 0]], ["humanities.csv", "GH", [0, 1, 0, 0, 0]],
+       ["health_wellness.csv", "GHW", [0, 0, 1, 0, 0]], ["nat_sci.csv", "GN", [0, 0, 0, 1, 0]],
+       ["social_behavior.csv", "GS", [0, 0, 0, 0, 1]]]
 for i in lst:
-    insert(i[0],i[1])
+    insert(i[0], i[1], i[2])

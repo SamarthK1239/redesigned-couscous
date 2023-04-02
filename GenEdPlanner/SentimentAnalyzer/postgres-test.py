104c152cbd5c84c1804525c179d65fdbb1f26b16
mayankyadavblr <prehistoric0001@gmail.com>
2023-04-01 23:45:20 -0400

    fuk yeahhhhh

--- GenEdPlanner/SentimentAnalyzer/postgres-test.py
+++ GenEdPlanner/SentimentAnalyzer/postgres-test.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
+import pymysql
 import pandas as pd
 import psycopg2 as psycopg2
 from dotenv import load_dotenv
 import os
-
+'''
 dotenv_path = Path('Environment Variables/.env')
 load_dotenv(dotenv_path=dotenv_path)
 
@@ -15,13 +16,33 @@ mydb = psycopg2.connect(
     database=os.getenv('database'),
     port=os.getenv('port')
 )
+'''
+conn=pymysql.connect(host='localhost',user='root',password='Mayank.123')
+cur=conn.cursor()
 
-cursor=mydb.cursor()
 
-stmt="desc subjects;"
-cursor.execute(stmt)
 
-#df_arts=pd.read_csv('arts_gened.csv')
 
-#for i in df_arts.iterrows():
+cur.execute("create database if not exists hackathonPSU;")
+cur.execute("use hackathonPSU;")
+cur.execute("drop table subjects;")
+cur.execute("CREATE TABLE IF NOT EXISTS subjects(Course VARCHAR(255), Code VARCHAR(10), Credits INT, GA INT, GHW INT, GH INT, GN INT, GS INT, Rating VARCHAR(15), ABSRating INT);")
 
+
+def insert(filename,types):
+    df_arts=pd.read_csv(filename)
+    print(df_arts)
+    for i in range(len(df_arts)):
+        print(df_arts.loc[i].at["Code"])
+        cur.execute('select * from subjects where code="'+df_arts.loc[i].at["Code"]+'";')
+        r=cur.rowcount
+        if r==0:
+            cur.execute("insert into subjects values('"+df_arts.loc[i].at['Course']+"','"+df_arts.loc[i].at['Code']+"','"+str(df_arts.loc[i].at['Credits'])+"',1,0,0,0,0,NULL,NULL);")
+            conn.commit()
+        else:
+            cur.execute("update subjects set "+types+"=1 where Code='"+df_arts.loc[i].at["Code"]+"';")  
+            conn.commit()
+
+lst=[["arts_gened.csv","GA"],["humanities.csv","GH"],["health_wellness.csv","GHW"],["nat_sci.csv","GN"],["social_behavior.csv","GS"]]
+for i in lst:
+    insert(i[0],i[1])
